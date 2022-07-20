a32a7cb933dc6aa46941aed2d1a7521e6d1a9035
sulavadhikari <cabbage.adhikari18@gmail.com>
2022-07-20 22:33:04 +0545

    switched to serializers

--- video/serializers.py
+++ video/serializers.py
@@ -1,8 +1,56 @@
 from rest_framework import serializers
+from moviepy.editor import VideoFileClip
+
+from videostorage.settings import UPLOAD_LIMIT
 from .models import Video
 
 class Videoserializer(serializers.ModelSerializer):\
 
+    title = serializers.CharField(read_only=True)
+    cost = serializers.IntegerField(read_only=True)
+    size = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Video
         fields = "__all__"
+
+    def calculate_cost(self, video, clip):
+        cost=0
+        if video.size < 500*1024*1024:
+            cost += 5
+        else:
+            cost+=12.5
+        if clip.duration > (6*60)+18:
+            cost+=20
+        return cost
+
+    def validate(self, data):
+        video = data.get('video')
+        if video.name.endswith('.mp4') or video.name.endswith('.mkv'):
+            
+            clip =  VideoFileClip(video.temporary_file_path())
+            duration = clip.duration
+            
+            if duration > 10*60:
+                raise serializers.ValidationError({
+                'message':'video exceeds length limit'
+                })
+            print(video.size)
+            if video.size > UPLOAD_LIMIT:
+                raise serializers.ValidationError({
+                'message':'file is too big'
+            })
+
+            cost = self.calculate_cost(video, clip)        
+            
+            obj = Video(title=video.name,video=video, size=video.size, cost=cost)
+            return{
+                'title':video.name,
+                'video':video,
+                'size':video.size,
+                'cost':cost,
+            }
+        else:
+            raise serializers.ValidationError({
+                'message':'invalid file type'
+            },)
--- video/views.py
+++ video/views.py
@@ -1,55 +1,22 @@
+from logging import raiseExceptions
 from django.shortcuts import render
 from rest_framework import status, views
 from rest_framework.response import Response 
 from moviepy.editor import VideoFileClip
 
 from videostorage.settings import UPLOAD_LIMIT
+from .serializers import Videoserializer
 from .models import Video
 
 # Create your views here.
 class FileUploadView(views.APIView):
     
-    def calculate_cost(self, video, clip):
-        cost=0
-        if video.size < 500*1024*1024:
-            cost += 5
-        else:
-            cost+=12.5
-        if clip.duration > (6*60)+18:
-            cost+=20
-        return cost
-
-
+    serializer_class = Videoserializer
+    
     def post(self, request):
-        video = request.FILES['video']
-        if video.name.endswith('.mp4') or video.name.endswith('.mkv'):
-            
-            clip =  VideoFileClip(video.temporary_file_path())
-            duration = clip.duration
-            
-            if duration > 10*60:
-                return Response({
-                'message':'video exceeds length limit'
-                }, status=400)
-            print(video.size)
-            if video.size > UPLOAD_LIMIT:
-                return Response({
-                'message':'file is too big'
-            }, status=400)
-
-            cost = self.calculate_cost(video, clip)        
-            
-            obj = Video(title=video.name,video=video, size=video.size, cost=cost)
-
-            
-            obj.save()
-
-
-            return Response({
-                'message':'file uploaded'
-            }, status=201)
-        else:
-            return Response({
-                'message':'invalid file type'
-            }, status=400)
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
         
